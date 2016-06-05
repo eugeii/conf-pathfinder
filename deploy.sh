91c3a7f30310aed58cb7bc0d0d9aaec8d0ffcf32
Eugene Ching <eugene@enegue.com>
2016-06-05 23:44:17 +0800

    Remove unnecessary copy in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cp -rf "./data/Library/Application Support/Path Finder/*" "$HOME/Library/Application Support/Path Finder/"
+# cp -rf "./data/Library/Application Support/Path Finder/*" "$HOME/Library/Application Support/Path Finder/"
 cp -rf "./data/Library/Preferences/com.cocoatech.pathfinder.plist" "$HOME/Library/Preferences/com.cocoatech.pathfinder.plist"
